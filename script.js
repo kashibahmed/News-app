00a486d733dc8076b9c88557d1996398011fa512
kashibahmed <ahmedkashib221@gmail.com>
2023-09-30 10:30:19 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "78d5a2013edf4011ab18d98e0d7dd804";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
